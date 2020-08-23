2c87370027a53707f360e558481f41c417ed3b90
dsgreve <dale@dalegreve.com>
2020-08-23 10:53:46 -0400

    started typography

---
+++ src/pages/typography.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "../components/header"
+export default function Typography() {
+  return (
+    <div class="bg-purple-700 min-h-screen">
+      <Header />
+      <h1>The Adventures of Sherlock Holmes</h1>
+      <h2>There's a cold-blooded scoundrel!</h2>
+      <h3>That fellow will rise from crime to crime until he does something very bad, and ends on a gallows.</h3>
+      <p>"What a woman--oh, what a woman!" cried the King of Bohemia, when we had all three read this epistle. "Did I not tell you how quick and resolute she was? Would she not have made an admirable queen? Is it not a pity that she was not on my level?"</p>
+      <p>"From what I have seen of the lady she seems indeed to be on a very different level to your Majesty," said Holmes coldly. "I am sorry that I have not been able to bring your Majesty's business to a more successful conclusion."</p>
+    </div>
+  )
+}
